f215a179aa63725608301d787d29086ed682d3fa
Tetiana <kortatiana89@gmail.com>
2020-11-22 16:18:06 +0200

    Changed a to div element

--- src/components/ClearTasks.js
+++ src/components/ClearTasks.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ClearTask = ({ handleClearTasks }) => {
     return (
         <div>
-            <a href="#" className="clear-tasks btn black" onClick={handleClearTasks}>Clear Tasks</a>
+            <div href="#" className="clear-tasks btn black" onClick={handleClearTasks}>Clear Tasks</div>
         </div>
     )
 }
